2ea88736f7549a34e00f25bf4f8ec0c18966a23b
Joe Kelly <jkelly@multivac.network>
2024-10-24 14:19:53 -0700

    changed organizations endpoint

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ async function main() {
 
     const gateway = new ApolloGateway({
         serviceList: [
-            {name: 'organizations', url: 'http://thrum-organizations-service.dev.svc.cluster.local/query'},
+            {name: 'organizations', url: 'http://thrum-organizations-service.default.svc.cluster.local/query'},
         ],
         buildService({name, url}) {
             return new RemoteGraphQLDataSource({
